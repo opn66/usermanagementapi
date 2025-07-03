2a18059fe4da3ad8986369836cbc8c6949d84a16
Om Prakash Nayak <omprakashnayak6666@gmail.com>
2025-07-03 11:49:24 +0530

    added paging

--- src/main/java/com/example/usermanagementapi/Controller/UserController.java
+++ src/main/java/com/example/usermanagementapi/Controller/UserController.java
@@ -4,6 +4,9 @@ import com.example.usermanagementapi.Model.User;
 import com.example.usermanagementapi.Repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -54,5 +57,10 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/page")
+    public Page<User> getUsers(Pageable pageable){
+        return userRepository.findAll(pageable);
     }
 }
